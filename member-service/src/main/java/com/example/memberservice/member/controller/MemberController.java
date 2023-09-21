25c41b2d0fab5be8bc192da15f625d073c6520c0
jgb1123 <rmsqja2@gmail.com>
2023-09-21 09:01:03 +0900

    [feat] Prometheus 및 Grafana 적용 #15

--- member-service/src/main/java/com/example/memberservice/member/controller/MemberController.java
+++ member-service/src/main/java/com/example/memberservice/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.memberservice.member.controller;
 import com.example.memberservice.member.dto.MemberPostDTO;
 import com.example.memberservice.member.dto.MemberResponseDTO;
 import com.example.memberservice.member.service.MemberService;
+import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +19,14 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/create")
+    @Timed(value = "members.create", longTask = true)
     public ResponseEntity<MemberResponseDTO> createMember(@RequestBody MemberPostDTO memberPostDTO) {
         MemberResponseDTO memberResponseDTO = memberService.createMember(memberPostDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(memberResponseDTO);
     }
 
     @GetMapping("/find/{memberId}")
+    @Timed(value = "members.find", longTask = true)
     public ResponseEntity<MemberResponseDTO> findMember(@PathVariable String memberId) {
         MemberResponseDTO memberResponseDTO = memberService.findMember(memberId);
         return ResponseEntity.status(HttpStatus.OK).body(memberResponseDTO);
